032e6b5274b4d5790a1bc276f3ccc45e4a49ea6a
jaime.siques <jaime.siques@gmail.com>
2017-05-29 16:31:07 -0400

    Picasso Agregado

---
+++ app/src/main/java/com/example/jaime/pecs2/GridviewAdapter.java
@@ -0,0 +1,58 @@
+package com.example.jaime.pecs2;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class GridviewAdapter extends BaseAdapter{
+
+    private Context context;
+    private Integer[] items;
+
+    public GridviewAdapter(Context context, Integer[] items){
+        super();
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView img;
+        if (convertView == null) {
+            img = new ImageView(context);
+            convertView = img;
+            img.setPadding(5, 5, 5, 5);
+            img.setAdjustViewBounds(true);
+
+        } else {
+            img = (ImageView) convertView;
+        }
+
+        Picasso.with(context)
+                .load(items[position])
+                .placeholder(R.drawable.cat1_abecedario)
+                .resize(200, 200)
+                .into(img);
+
+        return convertView;
+    }
+}

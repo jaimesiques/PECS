870d8b0a32ed06ea2277ddc57eb85b1b6ca4bcc4
jaimesiques <jaime.siques@gmail.com>
2018-04-17 16:41:34 -0300

    Resoluciones y sonido agregado

--- app/src/main/java/com/autumnideas/pecs_test/TirafraseActivity.java
+++ app/src/main/java/com/autumnideas/pecs_test/TirafraseActivity.java
@@ -2,6 +2,7 @@ package com.autumnideas.pecs_test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -11,12 +12,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Created by jaimearavena on 18-12-17.
  */
 
 public class TirafraseActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
+
+    String textoCompleto;
 
     CardView tirafrase_card1;
     CardView tirafrase_card2;
@@ -78,14 +84,177 @@ public class TirafraseActivity extends AppCompatActivity {
 
         //endregion
 
+
+        //region onClick Imagenes
+
+        tirafrase_card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Locale locSpanish = new Locale("spa", "MEX");
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(locSpanish);
+                            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                ConvertTextToSpeech(tirafrase_texto1.getText().toString());
+                                Log.e("texto", tirafrase_texto1.getText().toString());
+                            }
+                        } else
+                            Log.e("error", "Initilization Failed!");
+                    }
+                });
+                tts.setLanguage(locSpanish);
+            }
+        });
+
+
+        tirafrase_card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Locale locSpanish = new Locale("spa", "MEX");
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(locSpanish);
+                            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                ConvertTextToSpeech(tirafrase_texto2.getText().toString());
+                            }
+                        } else
+                            Log.e("error", "Initilization Failed!");
+                    }
+                });
+                tts.setLanguage(locSpanish);
+            }
+        });
+
+        tirafrase_card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Locale locSpanish = new Locale("spa", "MEX");
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(locSpanish);
+                            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                ConvertTextToSpeech(tirafrase_texto3.getText().toString());
+                            }
+                        } else
+                            Log.e("error", "Initilization Failed!");
+                    }
+                });
+                tts.setLanguage(locSpanish);
+            }
+        });
+
+        tirafrase_card4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Locale locSpanish = new Locale("spa", "MEX");
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(locSpanish);
+                            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                ConvertTextToSpeech(tirafrase_texto4.getText().toString());
+                            }
+                        } else
+                            Log.e("error", "Initilization Failed!");
+                    }
+                });
+                tts.setLanguage(locSpanish);
+            }
+        });
+
+        tirafrase_card5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Locale locSpanish = new Locale("spa", "MEX");
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(locSpanish);
+                            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                ConvertTextToSpeech(tirafrase_texto5.getText().toString());
+                            }
+                        } else
+                            Log.e("error", "Initilization Failed!");
+                    }
+                });
+                tts.setLanguage(locSpanish);
+            }
+        });
+
+        tirafrase_card6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Locale locSpanish = new Locale("spa", "MEX");
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        // TODO Auto-generated method stub
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = tts.setLanguage(locSpanish);
+                            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("error", "This Language is not supported");
+                            } else {
+                                ConvertTextToSpeech(tirafrase_texto6.getText().toString());
+                            }
+                        } else
+                            Log.e("error", "Initilization Failed!");
+                    }
+                });
+                tts.setLanguage(locSpanish);
+            }
+        });
+
+
+        //endregion
+
+
         initalizeLayout();
         loadData(args);
 
         asignData();
 
+        armarFrase();
     }
 
-    private void initalizeLayout(){
+    private void ConvertTextToSpeech(String text) {
+        // TODO Auto-generated method stub
+        if (text == null || "".equals(text)) {
+            text = "";
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        } else
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    private void initalizeLayout() {
 
         Tirafrase slot = new Tirafrase(tirafrase_image1, tirafrase_texto1, false);
         tirafrase_layout.add(slot);
@@ -106,16 +275,16 @@ public class TirafraseActivity extends AppCompatActivity {
         tirafrase_layout.add(slot);
     }
 
-    private void loadData(Bundle args){
+    private void loadData(Bundle args) {
 
-        for (int i = 0; i<=5; i++){
+        for (int i = 0; i <= 5; i++) {
 
-            if (args.getString("title"+i) != null){
+            if (args.getString("title" + i) != null) {
 
                 Log.e("i ?", "i : " + i);
 
-                int foto = args.getInt("image"+i);
-                String titulo = args.getString("title"+i);
+                int foto = args.getInt("image" + i);
+                String titulo = args.getString("title" + i);
 
                 PictoTirafrase pictoTirafrase = new PictoTirafrase(foto, titulo, true);
 
@@ -124,11 +293,11 @@ public class TirafraseActivity extends AppCompatActivity {
         }
     }
 
-    private void asignData(){
+    private void asignData() {
 
-        for(int i = 0; i < tirafrase.size(); i++){
+        for (int i = 0; i < tirafrase.size(); i++) {
 
-            if(!tirafrase_layout.get(i).getStatus()){
+            if (!tirafrase_layout.get(i).getStatus()) {
                 tirafrase_layout.get(i).setStatus(true);
                 tirafrase_layout.get(i).getImagen().setImageResource(tirafrase.get(i).getImage());
                 tirafrase_layout.get(i).getNombre().setText(tirafrase.get(i).getName());
@@ -136,10 +305,72 @@ public class TirafraseActivity extends AppCompatActivity {
         }
     }
 
-    public void backMain(View view){
-        Intent intent = new Intent (this, MainActivity.class);
+    public void backMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         finish();
         startActivity(intent);
     }
 
+    public void armarFrase() {
+        if (!tirafrase_texto1.getText().toString().equals("")) {
+            textoCompleto = tirafrase_texto1.getText().toString();
+            textoCompleto = textoCompleto + " ";
+            textoCompleto = textoCompleto + " ";
+            if (!tirafrase_texto2.getText().toString().equals("")) {
+                textoCompleto = textoCompleto + tirafrase_texto2.getText().toString();
+                textoCompleto = textoCompleto + " ";
+                textoCompleto = textoCompleto + " ";
+                if (!tirafrase_texto3.getText().toString().equals("")) {
+                    textoCompleto = textoCompleto + tirafrase_texto3.getText().toString();
+                    textoCompleto = textoCompleto + " ";
+                    textoCompleto = textoCompleto + " ";
+                    if (!tirafrase_texto4.getText().toString().equals("")) {
+                        textoCompleto = textoCompleto + tirafrase_texto4.getText().toString();
+                        textoCompleto = textoCompleto + " ";
+                        textoCompleto = textoCompleto + " ";
+                        if (!tirafrase_texto5.getText().toString().equals("")) {
+                            textoCompleto = textoCompleto + tirafrase_texto5.getText().toString();
+                            textoCompleto = textoCompleto + " ";
+                            textoCompleto = textoCompleto + " ";
+                            if (!tirafrase_texto6.getText().toString().equals("")) {
+                                textoCompleto = textoCompleto + tirafrase_texto6.getText().toString();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void reproducirFrase(View view) {
+        final Locale locSpanish = new Locale("spa", "MEX");
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(locSpanish);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                    else{
+                        ConvertTextToSpeech(textoCompleto);
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
+        tts.setLanguage(locSpanish);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(intent);
+        return;
+    }
 }
